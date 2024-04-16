3c933703c5b30e5102bdaf092e2b6e8d6e230708
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-04-16 18:30:30 -0300

    Criado GET para buscar todos os carros do banco

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -1,1 +1,21 @@
-const carroService = require('../services/CarroService')
+const carroService = require('../services/CarroService');
+
+async function buscarTodos(req, res) {
+    let json = {error:'', result:[]};
+
+    let carros = await carroService.buscarTodos();
+
+    // console.log(carros);
+
+    for (let i in carros) {
+        json.result.push({
+            codigo: carros[i].codigo,
+            descricao: carros[i].modelo
+        })
+    }
+    res.json(json);
+};
+
+module.exports = {
+    buscarTodos
+};
--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -1,5 +1,18 @@
 const db = require('../db')
 
-module.exports = {
+function buscarTodos() {
+    return new Promise((aceito, rejeitado) => {
+        db.query('SELECT * FROM carros', (error, results) => {
+            if (error) {
+                rejeitado(error);
+                return;
+            }
 
+            aceito(results);
+        })
+    });
+}
+
+module.exports = {
+    buscarTodos
 }
